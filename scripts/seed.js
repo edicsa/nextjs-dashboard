84030c65e90f22dff6fa93aab5f7109bcf22d870
edicsa <edi@toastermedia.net>
2024-10-14 21:35:08 +0200

    init
    init

--- scripts/seed.js
+++ scripts/seed.js
@@ -161,10 +161,10 @@ async function seedRevenue(client) {
 }
 
 async function main() {
-  console.log({
-    POSTGRES_URL: process.env.POSTGRES_URL,
-    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
-  });
+  // console.log({
+  //   POSTGRES_URL: process.env.POSTGRES_URL,
+  //   POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+  // });
 
   const client = await db.connect();
 
